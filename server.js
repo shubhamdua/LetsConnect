277ed49b26b75403c75c50ab87ab12bcb0a0cf00
shubhamdua <1997+shubhamdua@github.com>
2020-09-29 12:05:52 +0530

    Added some routes and User registration logic

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 
+const connectDb = require('./config/db');
+
 const app = express();
 
+//Connect to MongoDb Server
+connectDb();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API is running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
