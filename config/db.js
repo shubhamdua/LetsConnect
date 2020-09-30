0876b7082eda72c1dd39a36db76df9014142b565
shubhamdua <1997+shubhamdua@github.com>
2020-09-30 21:02:50 +0530

    Create,Update, Fetch Profile

--- config/db.js
+++ config/db.js
@@ -8,6 +8,7 @@ const connectDb = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
+      useFindAndModify: false,
     });
 
     console.log('MongoDb Connected....');
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,129 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
+const Profile = require('../../models/Profile');
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+// @route   GET api/profile/me
+// @desc    Get current user's profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error!!');
+  }
+});
+
+// @route   POST api/profile
+// @desc    Create or Update User's profile
+// @access  Private
+router.post(
+  '/',
+  auth,
+  [
+    check('role', 'Role is required').not().isEmpty(),
+    check('skills', 'Skills are required').not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      role,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    //Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (role) profileFields.role = role;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(',').map((skills) => skills.trim());
+    }
+
+    //Build social object
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true, upsert: true, setDefaultsOnInsert: true }
+      );
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error!!');
+    }
+  }
+);
 
 // @route   GET api/profile
-// @desc    Test Route
+// @desc    Get all profiles
 // @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+router.get('/all', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.msg);
+    res.status(500).send('Server Error!!');
+  }
+});
+
+// @route   GET api/profile/user/user_id
+// @desc    Get profile by user id
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not found!!' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found!!' });
+    }
+    res.status(500).send('Server Error!!');
+  }
+});
 
 module.exports = router;
